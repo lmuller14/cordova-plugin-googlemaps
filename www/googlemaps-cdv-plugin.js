c552b06b909b31464bcac6fcefa467d410fe9559
Lubos Muller <lmuller@trakm8.cz>
2017-09-25 11:37:51 +0200

    viewport-fit=cover added for iOS 11

--- www/googlemaps-cdv-plugin.js
+++ www/googlemaps-cdv-plugin.js
@@ -50,7 +50,22 @@ var saltHash = Math.floor(Math.random() * Date.now());
         viewportTag = document.createElement("meta");
         viewportTag.setAttribute('name', 'viewport');
     }
-    viewportTag.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no');
+
+    var viewportTagContent = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no';
+
+    // Detect if iOS device
+    if (/(iPhone|iPod|iPad)/i.test(window.navigator.userAgent)) {
+        // Get iOS major version
+        var iosVersion = parseInt((window.navigator.userAgent).match(/OS (\d+)_(\d+)_?(\d+)? like Mac OS X/i)[1]);
+        // Detect if device is running >iOS 11
+        // iOS 11's UIWebView and WKWebView changes the viewport behaviour to render viewport without the status bar. Need to override with "viewport-fit: cover" to include the status bar.
+        if (iosVersion >= 11) {
+            viewportTagContent += ', viewport-fit=cover';
+        }
+    }
+
+    // Update viewport tag attribute
+    viewportTag.setAttribute('content', viewportTagContent);
 })();
 
 /*****************************************************************************
